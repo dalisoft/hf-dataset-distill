ccfaf2fa7ad0e7799d4d28c889792a84c1af6063
Davlatjon Shavkatov <dalisoft.dev@gmail.com>
2026-03-12 20:35:43 +0500

    fix: types dir add

---
+++ types/app.d.ts
@@ -0,0 +1,22 @@
+interface IMessageEntry {
+  role: 'user' | 'assistant';
+  content: string;
+}
+interface IBatchResponse {
+  id: string;
+  archived_at: string;
+  cancel_initiated_at: string;
+  created_at: string;
+  ended_at: string;
+  expires_at: string;
+  processing_status: 'in_progress' | 'canceling' | 'ended';
+  request_counts: {
+    canceled: number;
+    errored: number;
+    expired: number;
+    processing: number;
+    succeeded: number;
+  };
+  results_url: string;
+  type: 'message_batch';
+}
